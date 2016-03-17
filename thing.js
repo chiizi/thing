8cc8b5c0a5127370b220b16c24346ecb80900967
Hans Felde <m33pfish@gmail.com>
2016-03-17 10:56:44 -0500

    Update thing.js

--- thing.js
+++ thing.js
@@ -1,3 +1,28 @@
 const _env = () => ({
-  
+  scope: [{}],
+  callStack: [],
+  enterScope(s) {
+    this.scope.push(s)
+  },
+  exitScope() {
+    this.scope.pop()
+  },
+  initialize(k, v) {
+    scope[scope.length - 1][k] = v
+  },
+  assign(k, v) {
+    var trigger
+    scope.reverse.forEach(s =>
+      (!trigger && k in s)
+        ? (scope[k] = v, trigger = true)
+        : null)
+  },
+  retreive(k) {
+    var ret, trigger
+    scope.reverse.forEach(s =>
+      (!trigger && k in s)
+        ? (ret = scope[k], trigger = true) // as problematic as it would be, a "cause caller to return value" operator would be great
+        : null)
+    return ret
+  }
 })
